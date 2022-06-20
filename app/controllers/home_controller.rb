50464b71b50ea96da39adc148e47dfb8d219b2f9
Daniel Olguin <daniel.olguin@gcabsa.com.mx>
2022-06-19 19:35:06 -0700

    cambios

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,28 +1,30 @@
 class HomeController < ApplicationController
+
   def index
     lines = Array.new
     lines2 = Array.new
 
-  File.open(Rails.root.join('storage', 'db.txt')).each { |line| lines << line }
-    
-  count = 0
-  array_tmp = Array.new
-    (1..1000).each do |va|
-      if lines2.count <= 10
-        count = count + 1
-
-        va1 = lines[rand(100..48633)].gsub("\;\n","")
-        if count <= 4
-          array_tmp << va1
-        else
-          lines2 << array_tmp
-          count = 0
-          array_tmp = Array.new
+    names = ["fuckcast","girl","woman","amateur","wife","pussy","latina","cajera","maduras","mom","esposa","mujer","chica",
+      "puta","nurse","linda","tits","tetas","vagina","pareja","vecina","nalgona","cunada","cuñada","novia","ex-novia","mexicana",
+      "oral","putinha","perra","golosa","nuas","morena","pussy","morrita","prima","tia","hermana","mama","morena","madura","mulher",
+      "she","sister","cousin","aunt"]
+    File.open(Rails.root.join('storage', '32860.txt')).each { |line| 
+      names.each { |e|
+        if line.downcase.include?(e)
+          lines << line
+          break
         end
-      end
+      }
+    }
+    
+    (1..12).each do |va|
+      va1 = lines[rand(1..14081)].gsub("\;\n","")
+      lines2 << va1
     end
+
     @json = lines2
     @info_video_json = ";Videos porno gratis, Free porn, Sex;;;;porn,sex,videos,xvideos,+18,erotic"
   end
 
 end
+
