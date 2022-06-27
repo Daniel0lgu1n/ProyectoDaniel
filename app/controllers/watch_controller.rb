479a52acd9422cbd2d872492094e5e695cb9b4c7
Daniel Olguin <daniel.olguin@gcabsa.com.mx>
2022-06-27 01:22:02 -0700

    cambios

--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -1,10 +1,24 @@
 class WatchController < ApplicationController
+
   def watch
-    lines = Array.new
 
-    File.open(Rails.root.join('storage', '14081.txt')).each { |line| lines << line }
+    begin
 
-    @info_video_json = lines.map { |va| va if va.include?(params["v"].split("_").first) }.compact.first
-    @codigo = params["v"].split("_").first
+      lines = Array.new
+
+      File.open(Rails.root.join('storage', '14081.txt')).each { |line| lines << line }
+
+      @info_video_json = lines.map { |va| va if va.include?(params["v"].split("_").first) }.compact.first
+      @codigo = params["v"].split("_").first
+
+      raise "error" if @info_video_json.blank?
+
+    rescue StandardError => e
+
+      redirect_to :controller => 'home', :action => 'index'
+
+    end
+    
   end
+
 end
