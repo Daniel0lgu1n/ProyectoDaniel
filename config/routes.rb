a85846566eb17e6f1f0b0d08564625b0d4d1d041
Daniel Olguin <daniel.olguin@gcabsa.com.mx>
2022-07-17 15:47:28 -0700

    cambios

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "watch", to: "watch#watch"
   get "/", to: "home#index"
   get '/sitemap.txt/' => 'home#sitemaps_txt'
-  get '/sitemap1.xml/:index' => 'home#sitemaps_xml', defaults: { index: "1" }
-  get '/sitemap2.xml/:index' => 'home#sitemaps_xml', defaults: { index: "2" }
+  get '/sitemap1.xml/' => 'home#sitemaps_xml', defaults: { index: "1" }
+  get '/sitemap2.xml/' => 'home#sitemaps_xml', defaults: { index: "2" }
   get '/robots.txt/' => 'home#robots'
 end
