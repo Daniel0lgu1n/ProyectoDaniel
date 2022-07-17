bac6bcf480687041a4f51514a36068e38487ad1f
Daniel Olguin <daniel.olguin@gcabsa.com.mx>
2022-07-17 04:48:32 -0700

    cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "watch", to: "watch#watch"
   get "/", to: "home#index"
-  get '/page-sitemap.txt/' => 'home#sitemaps_txt'
-  get '/page-sitemap.xml/' => 'home#sitemaps_xml'
+  get '/sitemap.txt/' => 'home#sitemaps_txt'
+  get '/sitemap.xml/' => 'home#sitemaps_xml'
   get '/robots.txt/' => 'home#robots'
 end
