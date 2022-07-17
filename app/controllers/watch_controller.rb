2d4904797d6a50c653ce325301c26523a478b48e
Daniel Olguin <daniel.olguin@gcabsa.com.mx>
2022-07-17 02:32:13 -0700

    cambios

--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -5,11 +5,23 @@ class WatchController < ApplicationController
     begin
 
       lines = Array.new
+      lines2 = Array.new
 
       File.open(Rails.root.join('storage', '14081.txt')).each { |line| lines << line }
 
       @info_video_json = lines.map { |va| va if va.include?(params["v"].split("_").first) }.compact.first
       @codigo = params["v"].split("_").first
+
+
+      (1..2).each do |va|
+        list_tmp = []
+        (1..3).each do
+          va1 = lines[rand(1..14081)].gsub("\;\n","")
+          list_tmp << va1
+        end
+        lines2 << list_tmp
+      end
+      @json = lines2
 
       raise "error" if @info_video_json.blank?
 
