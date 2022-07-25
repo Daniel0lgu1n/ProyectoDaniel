7396789487f42ac207f89c530ed62f46dff88969
Daniel Olguin <daniel.olguin@gcabsa.com.mx>
2022-07-24 17:01:07 -0700

    se agrega sitemaps

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   get '/new_sitemap1.txt/' => 'home#sitemaps_txt'
   get '/new_sitemap1.xml/' => 'home#sitemaps_xml', defaults: { index: "1" }
   get '/new_sitemap2.xml/' => 'home#sitemaps_xml', defaults: { index: "2" }
+  get '/new_sitemap3.xml/' => 'home#sitemaps_xml', defaults: { index: "3" }
+  get '/new_sitemap4.xml/' => 'home#sitemaps_xml', defaults: { index: "4" }
+  get '/new_sitemap5.xml/' => 'home#sitemaps_xml', defaults: { index: "5" }
+  get '/new_sitemap6.xml/' => 'home#sitemaps_xml', defaults: { index: "6" }
+  get '/new_sitemap7.xml/' => 'home#sitemaps_xml', defaults: { index: "7" }
+  get '/new_sitemap8.xml/' => 'home#sitemaps_xml', defaults: { index: "8" }
+  get '/new_sitemap9.xml/' => 'home#sitemaps_xml', defaults: { index: "9" }
+  get '/new_sitemap10.xml/' => 'home#sitemaps_xml', defaults: { index: "10" }
   get '/robots.txt/' => 'home#robots'
 end
